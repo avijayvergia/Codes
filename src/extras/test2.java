b476b27154c5a74c4f35337d7780f7bf45ba64d3
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-15 23:42:51 +0530

    Practice

--- src/extras/test2.java
+++ src/extras/test2.java
@@ -1,27 +1,52 @@
 package extras;
 
-import java.math.BigInteger;
-import java.util.Scanner;
+import org.omg.CORBA.NO_IMPLEMENT;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class test2 {
     public static void main(String[] args) {
-        Task solver = new Task();
-        solver.solve();
+        test2 tes = new test2();
+        tes.solve();
 
     }
 
-    static class Task {
+    class Node {
+        Node left, right;
+        int data;
 
-        void solve() {
-            Scanner scanner = new Scanner(System.in);
-
-            int t = scanner.nextInt();
-            BigInteger x, y;
-            for (int i = 0; i < t; i++) {
-                x = scanner.nextBigInteger();
-                y = scanner.nextBigInteger();
-                System.out.println(x.add(y));
-            }
+        Node(int x) {
+            this.data = x;
+            left = null;
+            right = null;
         }
     }
+
+    private void find(Node x) {
+        Queue<HashMap<Node, Integer>> queue = new LinkedList<>();
+        HashMap<Node, Integer> map = new HashMap<>();
+        map.put(x, 0);
+        queue.add(map);
+        Node a;
+        int level;
+        while (!queue.isEmpty()) {
+            map=queue.poll();
+
+        }
+    }
+
+    private void solve() {
+        Node root = new Node(6);
+        root.left = new Node(4);
+        root.left.left = new Node(3);
+        root.left.right = new Node(5);
+        root.right = new Node(7);
+        root.right.left = new Node(8);
+        root.right.right = new Node(9);
+        find(root);
+    }
+
+
 }
