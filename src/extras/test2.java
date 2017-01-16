a3bb1225f67368c9fb6e835a631468f05fd0e2b0
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-16 23:06:35 +0530

    Linked List

--- src/extras/test2.java
+++ src/extras/test2.java
@@ -1,52 +1,66 @@
 package extras;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class test2 {
     public static void main(String[] args) {
         test2 tes = new test2();
         tes.solve();
     }
 
-    private ArrayList<String> list;
+    private Node head = null;
 
-    private void perm(char[] a, int i, int n) {
-        if (i == n) {
-            list.add(Arrays.toString(a));
-            return;
-        }
-        for (int j = i; j <= n; j++) {
-            swap(a, i, j);
-            perm(a, i + 1, n);
-            swap(a, i, j);
+    class Node {
+        Node next;
+        int data;
+
+        Node(int x) {
+            this.data = x;
+            next = null;
         }
     }
 
-    private void swap(char[] a, int i, int j) {
-        char temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    private void insert(int x) {
+
+        if (head == null) {
+            head = new Node(x);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(x);
+        }
+    }
+
+    private void reverse(){
+        Node curr=head;
+        Node prev=null,next;
+
+       while (curr!=null){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+       }
+       head=prev;
+
+    }
+
+    private void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
 
     private void solve() {
-        char[] a = {'d', 'r', 'r'};
-        list = new ArrayList<>();
-        int[][] mat={{1,-3,0},{-2,1,-1}};
-        perm(a, 0, 2);
-        int max=0;
-        int x=0,y=0;
-        for (String s : list) {
-            int sum=0;
-            for(int i=0;i<s.length();i++){
-                if(s.charAt(i)=='d') x++;
-                else y++;
-                sum+=mat[x][y];
-            }
-            max=Math.max(max,sum+mat[0][0]);
-        }
-        System.out.println(max);
-
+        insert(1);
+        insert(2);
+        insert(3);
+        traverse();
+        reverse();
+        traverse();
     }
 
 
