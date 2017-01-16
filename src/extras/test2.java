5912332cb7fb7ed590da4b3aa625a621e3f4598d
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-16 23:09:26 +0530

    Doubly Linked List

--- src/extras/test2.java
+++ src/extras/test2.java
@@ -1,67 +1,40 @@
 package extras;
 
 public class test2 {
+
+    private int[] next;
+    private int[] prev;
+
+    public test2(int maxSize) {
+        next = new int[maxSize + 1];
+        prev = new int[maxSize + 1];
+    }
+
+    public void insert(int x, int pos) {
+        prev[x] = pos;
+        next[x] = next[pos];
+        prev[next[x]] = x;
+        next[prev[x]] = x;
+    }
+
+    public void remove(int x) {
+        next[prev[x]] = next[x];
+        prev[next[x]] = prev[x];
+    }
+
+    // Usage example
     public static void main(String[] args) {
-        test2 tes = new test2();
-        tes.solve();
-    }
-
-    private Node head = null;
-
-    class Node {
-        Node next;
-        int data;
-
-        Node(int x) {
-            this.data = x;
-            next = null;
+        int n = 10;
+        test2 list = new test2(n);
+        for (int i = 1; i <= n; i++) {
+            list.insert(i, 0);
         }
-    }
-
-    private void insert(int x) {
-
-        if (head == null) {
-            head = new Node(x);
-        } else {
-            Node temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = new Node(x);
-        }
-    }
-
-    private void reverse(){
-        Node curr=head;
-        Node prev=null,next;
-
-       while (curr!=null){
-           next=curr.next;
-           curr.next=prev;
-           prev=curr;
-           curr=next;
-       }
-       head=prev;
-
-    }
-
-    private void traverse() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
+        list.remove(1);
+        list.remove(10);
+        list.remove(5);
+        for (int i = list.next[0]; i != 0; i = list.next[i]) {
+            System.out.print(i + " ");
         }
         System.out.println();
     }
-
-    private void solve() {
-        insert(1);
-        insert(2);
-        insert(3);
-        traverse();
-        reverse();
-        traverse();
-    }
-
-
 }
