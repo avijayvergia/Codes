71ec694a02d6c59d540021d6aec12949cc8f200e
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-17 23:16:51 +0530

    Max Subarray

--- src/extras/test.java
+++ src/extras/test.java
@@ -1,38 +1,24 @@
 package extras;
 
 public class test {
-    private int[] b;
-
     public static void main(String[] args) {
         test tree = new test();
         tree.solve();
     }
 
-    private void solve() {
-        int[] a = {5, 2, 3, 6, 7};
-        b = new int[]{9, 2, 8, 4, 5};
-        System.out.println(find(a, 0, 0));
-    }
+    public void solve(){
+        int[] a={1,-5,4,8,-78,89,-89,70,10,10,-80};
+        // MaxSum subarray
 
-    private int find(int[] a, int index, int d) {
-        if (index == 4) {
-            int x = 0;
-            for (int i = 0; i < 5; i++) {
-                x = x + a[5 - i - 1] * (int) Math.pow(10, i);
-            }
-            return x;
+        int curr_max=0;
+        int max_sum=0;
+        for (int anA : a) {
+            curr_max = Math.max(curr_max + anA, anA);
+            max_sum = Math.max(curr_max, max_sum);
         }
-        int r_max = find(a, index + 1, d);
-        for (int i = d; i < 5; i++) {
-            r_max = Math.max(r_max, find(swap(a, i, index), index + 1, i + 1));
-        }
-        return r_max;
-    }
 
-    private int[] swap(int[] a, int i, int index) {
-        int[] x = a.clone();
-        x[index] = b[i];
-        return x;
+        System.out.println(max_sum);
+
     }
 
 }
