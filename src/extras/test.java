f002e9ed7cdebe19469eab14812f0eb9487a2d5a
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-17 23:48:54 +0530

    Integer Knapsack

--- src/extras/test.java
+++ src/extras/test.java
@@ -8,28 +8,28 @@ public class test {
         tree.solve();
     }
 
-    public void solve(){
-        int[] a={1,-5,4,8,-78,89,90,70,10,10,-80};
-        // Largest Increasing Subsequence
+    private int[][] array;
 
-        int[] len=new int[a.length];
-        Arrays.fill(len,1);
-
-        for(int i=1;i<a.length;i++){
-            for(int j=0;j<i;j++){
-                if(a[i]>a[j]&&len[i]<len[j]+1){
-                    len[i]=len[j]+1;
-                }
-            }
+    public void solve() {
+        int val[] = new int[]{60, 100, 120,221};
+        int wt[] = new int[]{10, 20, 30,50};
+        int W = 50;
+        array = new int[5][51];
+        for(int i=0;i<4;i++){
+            Arrays.fill(array[i], -1);
         }
 
-        int max=0;
-        for(int i=0;i<a.length;i++){
-            if(max<len[i]) max=len[i];
-        }
-        System.out.println(max);
+        System.out.println(knapsack(wt, val, W, 0));
 
+    }
 
+    private int knapsack(int[] wt, int[] val, int w, int i) {
+
+        if (w == 0 || i == 4) return 0;
+        if (array[i][w] != -1) return array[i][w];
+        if (wt[i] > w) return knapsack(wt, val, w, i + 1);
+        array[i][w] = Math.max(val[i] + knapsack(wt, val, w - wt[i], i + 1), knapsack(wt, val, w, i + 1));
+        return array[i][w];
     }
 
 }
