2d3ac5d6dd5e0bff351a2a41e95713b832fabd17
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-18 23:25:15 +0530

    Leaf Linked List

--- src/extras/test.java
+++ src/extras/test.java
@@ -1,37 +1,65 @@
 package extras;
 
-import java.util.Arrays;
-
 public class test {
     public static void main(String[] args) {
         test tree = new test();
         tree.solve();
     }
 
-    private int[][] array;
+    private Node head=null;
+    private Node curr=null;
 
     public void solve() {
-        int val[] = new int[]{60, 100, 120,221};
-        int wt[] = new int[]{10, 20, 30,50};
-        int W = 50;
-        array = new int[5][51];
-        for(int i=0;i<4;i++){
-            Arrays.fill(array[i], -1);
+        Node root = new Node(6);
+        root.left = new Node(4);
+        root.left.left = new Node(3);
+        root.left.right = new Node(5);
+        root.left.left.left = new Node(2);
+        root.right = new Node(10);
+        root.right.left = new Node(9);
+        root.right.right = new Node(12);
+        traverse(root);
+        print(head);
+    }
+
+    private void print(Node root) {
+        curr=root;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.right;
         }
-
-        System.out.println(knapsack(wt, val, W, 0));
-
     }
 
-    private int knapsack(int[] wt, int[] val, int w, int i) {
-
-        if (w == 0 || i == 4) return 0;
-        if (array[i][w] != -1) return array[i][w];
-        if (wt[i] > w) return knapsack(wt, val, w, i + 1);
-        array[i][w] = Math.max(val[i] + knapsack(wt, val, w - wt[i], i + 1), knapsack(wt, val, w, i + 1));
-        return array[i][w];
+    private Node traverse(Node x) {
+        if(x==null) return null;
+        if(x.left==null&&x.right==null){
+            if(head==null){
+                head=x;
+                curr=head;
+            }
+            else{
+                curr.right=x;
+                curr=x;
+            }
+            return null;
+        }
+        x.left=traverse(x.left);
+        x.right=traverse(x.right);
+        return x;
     }
 
+
+
+    class Node {
+        Node left, right;
+        int data;
+
+        Node(int x) {
+            this.data = x;
+            left = null;
+            right = null;
+        }
+    }
 }
 
 
