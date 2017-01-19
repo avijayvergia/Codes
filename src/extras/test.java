3c35e10e997163bdf281fe6edd1d5e3d8dd587b3
Akshay Vijayvergia <akshay.go2014@gmail.com>
2017-01-19 18:52:51 +0530

    Insertion Sort

--- src/extras/test.java
+++ src/extras/test.java
@@ -7,41 +7,30 @@ class test {
     }
 
     private void solve() {
-        int[] a={5,8,59,-4,7,0,9};
-        sort(a,0,a.length-1);
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        int[] a = {5, 8, 59, -4, 7, 0, 9};
+        sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 
-    private void sort(int[] a, int low, int high) {
-        if(low==high) return;
-        int mid=(low+high)/2;
-        sort(a,low,mid);
-        sort(a,mid+1,high);
-        merge(a,low,high,mid);
-    }
-
-    private void merge(int[] a, int low, int high, int mid) {
-        int [] temp=a.clone();
-        int i=low;
-        int j=mid+1;
-        int count=low;
-        while (i<=mid&&j<=high){
-            if(temp[i]<temp[j]){
-                a[count]=temp[i++];
+    private void sort(int[] a) {
+        int num;
+        for (int i = 1; i < a.length; i++) {
+            num = a[i];
+            int j;
+            for ( j = i-1; j>=0&&a[j]>num; j--) {
+                    a[j+1]=a[j];
             }
-            else a[count]=temp[j++];
-            count++;
-        }
-        while (i<=mid){
-            a[count]=temp[i++];
-            count++;
-        }
-        while (j<=high){
-            a[count]=temp[j++];
-            count++;
+            a[j + 1] = num;
         }
     }
+
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
 
 }
